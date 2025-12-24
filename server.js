812b10acc180139c3cc54601cd05852085b93a8b
GitHubPlayer0 <65072506+GitHubPlayerZero@users.noreply.github.com>
2025-12-24 19:22:40 +0800

    調整 server.js 的端口設定，以適用於 Zeabur

--- server.js
+++ server.js
@@ -1,19 +1,23 @@
 // See https://github.com/typicode/json-server#module
 const cors = require('cors');
-const jsonServer = require('json-server')
-const server = jsonServer.create()
+const jsonServer = require('json-server');
+const server = jsonServer.create();
 const auth = require("json-server-auth");
 const db = require("./db.json");
 const router = jsonServer.router(db);
 const middlewares = jsonServer.defaults();
-server.use(cors())
-server.use(middlewares)
+
+server.use(cors());
+server.use(middlewares);
 server.db = router.db;
 server.use(auth);
-server.use(router)
-server.listen(3000, () => {
-    console.log('JSON Server is running')
+server.use(router);
+
+// 使用環境變數的 port，或預設 3000
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+  console.log(`JSON Server is running on port ${port}`)
 })
 
 // Export the Server API
-module.exports = server
+module.exports = server;
